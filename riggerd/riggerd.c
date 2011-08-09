52f3efe0c8f47ebcc8b181d4258ad35ab51ed56f
Wouter Wijngaards <wouter@nlnetlabs.nl>
2011-08-09 12:25:47 +0000

    hooks
    git-svn-id: file:///svn/dnssec-trigger/trunk@7 14dc9c71-5cc2-e011-b339-0019d10b89f4

--- riggerd/riggerd.c
+++ riggerd/riggerd.c
@@ -110,6 +110,43 @@ unlink_pid(char* pidfile)
         unlink(pidfile);
 }
 
+/** detach from command line */
+static void
+detach(void)
+{
+#if defined(HAVE_DAEMON) && !defined(DEPRECATED_DAEMON)
+        /* use POSIX daemon(3) function */
+        if(daemon(1, 0) != 0)
+                fatal_exit("daemon failed: %s", strerror(errno));
+#else /* no HAVE_DAEMON */
+#ifdef HAVE_FORK
+        int fd;
+        /* Take off... */
+        switch (fork()) {
+                case 0:
+                        break;
+                case -1:
+                        fatal_exit("fork failed: %s", strerror(errno));
+                default:
+                        /* exit interactive session */
+                        exit(0);
+        }
+        /* detach */
+#ifdef HAVE_SETSID
+        if(setsid() == -1)
+                fatal_exit("setsid() failed: %s", strerror(errno));
+#endif
+        if ((fd = open("/dev/null", O_RDWR, 0)) != -1) {
+                (void)dup2(fd, STDIN_FILENO);
+                (void)dup2(fd, STDOUT_FILENO);
+                (void)dup2(fd, STDERR_FILENO);
+                if (fd > 2)
+                        (void)close(fd);
+        }
+#endif /* HAVE_FORK */
+#endif /* HAVE_DAEMON */
+}
+
 /** do main work of daemon */
 static void
 do_main_work(const char* cfgfile, int nodaemonize, int verb)
@@ -141,8 +178,7 @@ do_main_work(const char* cfgfile, int nodaemonize, int verb)
         if(!svr) fatal_exit("could not init server");
         log_init(cfg->logfile, cfg->use_syslog, cfg->chroot);
         if(!nodaemonize)
-                if(daemon(1, 0) != 0)
-                        fatal_exit("could not daemonize: %s", strerror(errno));
+		detach();
         store_pid(cfg->pidfile);
 	log_info("%s start", PACKAGE_STRING);
         svr_service(svr);
