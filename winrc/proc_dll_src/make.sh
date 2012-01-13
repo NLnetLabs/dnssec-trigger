865db017d165d6f5c830a16d3683a47bfbd7d59b
Wouter Wijngaards <wouter@nlnetlabs.nl>
2012-01-13 14:09:02 +0000

    mention Andrei Ciubotaru, maker of Processes.dll.
    git-svn-id: file:///svn/dnssec-trigger/trunk@436 14dc9c71-5cc2-e011-b339-0019d10b89f4

--- winrc/proc_dll_src/make.sh
+++ winrc/proc_dll_src/make.sh
@@ -1,6 +1,7 @@
 #i686-pc-mingw32-gcc processes.cpp -o bla.dll -mdll -nodefaultlibs -luser32 -lgcc -lmoldname -lmingw32 -lmsvcrt -lkernel32
-set -v
+echo gcc
 i686-pc-mingw32-gcc -g -O2 processes.c -o proc.dll -mdll -nostartfiles -e __DllMainCRTStartup@12
 ls -l proc.dll
+echo strip
 i686-pc-mingw32-strip proc.dll
 ls -l proc.dll
